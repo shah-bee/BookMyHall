056fd8b0a810cc0920831bcf553af45619c98aa4
Mohammed Shah Taher <taher.s@COMAKEIT.COM>
2017-08-01 15:57:16 +0530

    Adding angular2 to app

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const appRoutes:Routes = [
+  { path:'', component : HomeComponent},
+  { path:'login', component : LoginComponent},
+  { path:'register', component : RegisterComponent},
+  { path:'dashboard', component : DashboardComponent},
+  { path:'profile', component : ProfileComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
